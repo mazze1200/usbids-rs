348fde0f4b41e803b02911b123f457ba654764d1
Mathias <mathias.boesl@gmx.de>
2017-06-23 20:41:50 +0200

    add functions to get vendor names and product names

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,15 @@ use regex::Regex;
 use std::collections::HashMap;
 
 pub struct USBIDs {
-    ids: HashMap<u32, Vendor>,
+    ids: HashMap<u16, Vendor>,
 }
 
 struct Vendor {
-    id: u32,
     name: String,
-    products: HashMap<u32, Product>,
+    products: HashMap<u16, Product>,
 }
 
 struct Product {
-    id: u32,
     name: String,
 }
 
@@ -24,9 +22,8 @@ pub struct USBResult {
 }
 
 impl Vendor {
-    fn new(id: u32, name: &str) -> Vendor {
+    fn new(name: &str) -> Vendor {
         Vendor {
-            id: id,
             name: name.to_string(),
             products: HashMap::new(),
         }
@@ -34,11 +31,8 @@ impl Vendor {
 }
 
 impl Product {
-    fn new(id: u32, name: &str) -> Product {
-        Product {
-            id: id,
-            name: name.to_string(),
-        }
+    fn new(name: &str) -> Product {
+        Product { name: name.to_string() }
     }
 }
 
@@ -47,38 +41,38 @@ impl USBIDs {
         let vendor_regex = Regex::new(r"^([[:xdigit:]]{4})  (.+)$").unwrap();
         let product_regex = Regex::new(r"^	([[:xdigit:]]{4})  (.+)$").unwrap();
 
-        let mut vendors: HashMap<u32, Vendor> = HashMap::new();
+        let mut vendors: HashMap<u16, Vendor> = HashMap::new();
 
-        let mut vendor_id = 0;
+        let mut vendor_id: u16 = 0;
 
         let source = include_str!("usb.ids").lines();
 
         for line in source {
             if !line.is_empty() {
                 if let Some(vendor) = vendor_regex.captures(line) {
-                    let id = vendor
+                    let id: u16 = vendor
                         .get(1)
-                        .map(|m| u32::from_str_radix(m.as_str(), 16))
+                        .map(|m| u16::from_str_radix(m.as_str(), 16))
                         .unwrap()
                         .unwrap();
 
                     let name = vendor.get(2).map(|m| m.as_str()).unwrap();
 
-                    let temp_vendor = Vendor::new(id, name);
+                    let temp_vendor = Vendor::new(name);
                     vendor_id = id;
 
                     vendors.insert(id, temp_vendor);
                 } else if let Some(product) = product_regex.captures(line) {
-                    let id = product
+                    let id: u16 = product
                         .get(1)
-                        .map(|m| u32::from_str_radix(m.as_str(), 16))
+                        .map(|m| u16::from_str_radix(m.as_str(), 16))
                         .unwrap()
                         .unwrap();
 
                     let name = product.get(2).map(|m| m.as_str()).unwrap();
 
                     if let Some(v) = vendors.get_mut(&vendor_id) {
-                        let product = Product::new(id, name);
+                        let product = Product::new(name);
                         v.products.insert(id, product);
                     }
                 }
@@ -87,7 +81,7 @@ impl USBIDs {
         USBIDs { ids: vendors }
     }
 
-    pub fn get_usb_id(&self, vendor_id: u32, product_id: u32) -> Option<USBResult> {
+    pub fn get_usb_id(&self, vendor_id: u16, product_id: u16) -> Option<USBResult> {
         if let Some(v) = self.ids.get(&vendor_id) {
             if let Some(p) = v.products.get(&product_id) {
                 return Some(USBResult {
@@ -99,6 +93,24 @@ impl USBIDs {
                                 vendor: v.name.clone(),
                                 product: String::from("<UNKNOWN_PRODUCT>"),
                             });
+            }
+        }
+
+        None
+    }
+
+    pub fn get_vendor_name(&self, vendor_id: u16) -> Option<String> {
+        if let Some(v) = self.ids.get(&vendor_id) {
+            return Some(v.name.clone());
+        }
+
+        None
+    }
+
+    pub fn get_product_name(&self, vendor_id: u16, product_id: u16) -> Option<String> {
+        if let Some(v) = self.ids.get(&vendor_id) {
+            if let Some(p) = v.products.get(&product_id) {
+                return Some(p.name.clone());
             }
         }
 
